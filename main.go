05cb4aba507ca074b9d96d7efafe20d24b5d760f
Roger <rdpender3@gmail.com>
2019-01-12 20:02:14 -0800

    adding notes

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	// only use := when defining a new variable
+	// can just assign new values to card after the first :=
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
 }
