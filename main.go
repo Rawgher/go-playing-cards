be46d21fb12272a8bc93805ba960fa1f873b8ca5
Roger <rdpender3@gmail.com>
2019-01-12 20:22:18 -0800

    added slices and for loops

--- main.go
+++ main.go
@@ -3,11 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
+
 	// only use := when defining a new variable
 	// can just assign new values to card after the first :=
-	card = "Five of Diamonds"
+	//  ex: card = "Five of Diamonds"
 
-	fmt.Println(card)
+	// cards := []string{newCard(), newCard()}
+	cards := []string{"Ace of Diamonds", newCard()}
+
+	cards = append(cards, "Six of Spades")
+	// append will add the card to the slice, making a new slice from the first one declared
+
+	// how to iterate over the slice cards
+	// for loop that remakes card every time it iterates
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
 }
+
+func newCard() string {
+	return "Five of Diamonds"
+}
+
+// array is a fixed length list of values
+// slices are arrays that can grow and shrink
+// all things in arrays or slices must be the same type (string, int, bool, float64)
+// [] = slice
